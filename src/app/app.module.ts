66021a81f57b65ffd0c8c2d1b5ab8b2e0b789138
Pinky <alejandro.hernandez@finerio.mx>
2021-11-01 22:40:54 -0600

    added nav bar bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PortfolioComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
