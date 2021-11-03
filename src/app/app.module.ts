94e3cb76e856aafa639ac0271cd8b0e4684556f2
Pinky <alejandro.hernandez@finerio.mx>
2021-11-02 20:57:13 -0600

    added galery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {IvyGalleryModule} from 'angular-gallery';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { WorkComponent } from './work/work.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IvyGalleryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Gallery} from 'angular-gallery';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gallery: Gallery) {}
+
+  showGallery(index: number = 0) {
+      let prop: any = {};
+      prop.images = [
+          {path: '/assets/photo-1547691889-841a6f1c5ca6.jpg'},
+          {path: '/assets/photo-1548625149-9129dad5eef7.jpg'},
+          {path: '/assets/photo-1548625149-d37da68f9a7f.jpg'},
+          {path: '/assets/photo-1487819162750-4a48cfbf83b0.jpg'},
+          {path: '/assets/photo-1489365091240-6a18fc761ec2.jpg'}
+      ];
+      prop.index = index;
+      this.gallery.load(prop);
+  }
+
+  closeGallery() {
+      this.gallery.close();
+  }
 
   ngOnInit(): void {
   }
