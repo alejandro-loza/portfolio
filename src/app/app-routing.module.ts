8a0f418241b201bee884560ff803fbaa8ee4b2a0
Pinky <alejandro.hernandez@finerio.mx>
2021-11-02 20:13:56 -0600

    routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { WorkComponent } from './work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/portfolio', pathMatch: 'full' },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'work', component: WorkComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
